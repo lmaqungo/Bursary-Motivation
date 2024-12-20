f68cfb1d62a0f38f1919ac0df6975e9745d2a7b7
Luyanda <lmaqungo1@gmail.com>
2024-12-21 01:31:41 +0200

    minor fixes

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-const spans = document.querySelectorAll("span");
 const colourBtn = document.querySelector("button");
+const content = document.querySelector(".copy");
+const paragraphs = document.querySelectorAll(".text");
+let index = 0;
+
+function changeColour() {
+
+    for(let para of paragraphs){
+        let text = para.textContent;
+        let words = text.split(/\s+/).map(word => `<span>${word}</span>`).join(" ");
+        para.innerHTML = words;
+    }
+
+    const rawSpans = [...content.querySelectorAll('span')];
+
+    const spans = rawSpans.filter(function(span) { return span.textContent.length >= 1;});
+
+    if (index >= spans.length){
+        index = 0;
+    }
+
+    const currentColor = randomRGB();
+    spans[index].style.color = currentColor;
+    colourBtn.style.boxShadow = `5px 5px ${currentColor}`
+    
+
+    index++;
+}
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
 function randomRGB() {
 return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
-
-function brightness(htmlElement){
-    const colour = htmlElement.style.backgroundColor;
-    const rgbValues = colour.match(/\d+/g);
-
-    let r = parseInt(rgbValues[0]);
-    let g = parseInt(rgbValues[1]);
-    let b = parseInt(rgbValues[2]);
-
-    r = r/255;
-    g = g/255;
-    b = b/255;
-    
-    const brightnessVal = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-
-    if (brightnessVal > 0.5){
-        htmlElement.style.color = "#213555"; 
-    }
-    else{
-        htmlElement.style.color = "#F8FAFC";
-    }
-}
-
-function changeColour(){
-    for (let span of spans){
-        span.style.color = randomRGB();
-    }
-    colourBtn.style.backgroundColor = randomRGB();
-    brightness(colourBtn);
 }
 
 colourBtn.addEventListener('click', changeColour);
